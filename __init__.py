d2a76f4eb5baf736354e9e9f4a45f13be1f66850
SeungHan Moon <msh9911@naver.com>
2024-05-10 00:55:34 +0900

    Hi

--- __init__.py
+++ __init__.py
@@ -180,7 +180,7 @@ class MeshMetryOperator(bpy.types.Operator):
             else:
                 bpy.ops.mesh.select_random(ratio=percentage / 100, seed=randint(1, 9999))
 
-        def randomgrowshrink(percentage, min, max):
+        def randomgs(percentage, min, max):
             randomselect(percentage)
             for j in range(min, max):
                 bpy.ops.mesh.select_more()
@@ -216,7 +216,7 @@ class MeshMetryOperator(bpy.types.Operator):
                 print()
 
                 # select random & grow
-                randomgrowshrink(0.15, 0, grow)
+                randomgs(0.15, 0, grow)
                 if grow > 10:
                     max += 1
 
@@ -260,7 +260,7 @@ class MeshMetryOperator(bpy.types.Operator):
             if rmp.decimate == True:
                 # vgroup
                 bom.select_all(action='DESELECT')
-                randomgrowshrink(0.5, 5, 8)
+                randomgs(0.5, 5, 8)
                 boo.vertex_group_add()
                 boo.vertex_group_assign()
                 boo.vertex_group_smooth(repeat=randint(0, 10))
